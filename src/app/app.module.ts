5f1b4424a4f71cd82e40b20aa48a0ddb4671947f
danielxacosta <52141384+danielxacosta@users.noreply.github.com>
2020-09-16 18:05:57 -0500

    Inicialización proyecto tuiik

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TiendaComponent } from './tienda/tienda.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ListaDeseosComponent } from './lista-deseos/lista-deseos.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TiendaComponent,
+    BlogComponent,
+    ContactoComponent,
+    ListaDeseosComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
