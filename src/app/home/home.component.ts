f83e53b3f31a6b6656ded6ff426f4a5cd6231f12
SamHaroldGarcia <samdeveloperhg@gmail.com>
2020-09-18 18:45:05 -0500

    Nuevos Cambios Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  data = [];
+  customOptions = {};
+
   constructor() { }
 
   ngOnInit(): void {
+    this.customOptions = {
+      loop: true,
+      center: true,
+      mouseDrag: true,
+      touchDrag: true,
+      pullDrag: true,
+      dots: true,
+      navSpeed: 1500,
+      autoplay: false,
+      autoplayHoverPause: true,
+      autoplaySpeed: 1500,
+      dotsSpeed: 1500,
+      navText: ['', ''],
+      responsive: {
+        0: {
+          items: 1
+        },
+        400: {
+          items: 3
+        },
+        740: {
+          items: 5
+        },
+      },
+      nav: true
+    };
+    this.cargarData();
+  }
+  cargarData() {
+    this.data = [
+      {
+        imgCarousel: 'imgCarousel',
+      },
+      {
+        imgCarousel: 'imgCarousel',
+      },
+      {
+        imgCarousel: 'imgCarousel',
+      },
+      {
+        imgCarousel: 'imgCarousel',
+      },
+      {
+        imgCarousel: 'imgCarousel',
+      },
+      {
+        imgCarousel: 'imgCarousel',
+      },
+      {
+        imgCarousel: 'imgCarousel',
+      },
+    ];
   }
 
 }
